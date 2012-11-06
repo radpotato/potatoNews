a164ec389ddbafd9bfc72a76568890b92742a85e
potato <apes0113@nate.com>
2012-11-06 15:02:55 +0900

    server modi

--- dataServer/server.js
+++ dataServer/server.js
@@ -1,10 +1,11 @@
 var http = require('http')
     , fs = require('fs');
-http.createServer(function (req, res) {
-	fs.readFile('./viewer/index.html', function(error, data) {
-		res.writeHead(200, { 'Content-Type' : 'text/html'});
+var requestListener = function(req, res) {
+    fs.readFile('./viewer/index.html', function(error, data) {
+        res.writeHead(200, { 'Content-Type' : 'text/html'});
 		res.end(data);
-	});
-}).listen(3000, '127.0.0.1');
+    });
+};
+var server = http.createServer(requestListener);
 
-console.log('Server running at http://127.0.0.1:3000');
+server.listen(process.env.PORT || 3000, "127.0.0.1");
